9b0fc877b49b23f05edcbdee7b16ba4167329012
Mazen Afifi <mazen.afifi@flairstech.com>
2024-11-04 19:02:05 +0200

    push branch try

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -3,6 +3,7 @@ import UserProfileForm from "@/forms/user-profile-form/UserProfileForm"
 
 export const UserProfilePage = () => {
     const { isLoading, updateUser } = useUpdateMyUser()
+    
     return <UserProfileForm onSave={updateUser} isLoading={isLoading} />
 
 }
